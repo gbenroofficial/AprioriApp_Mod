85aeb936c5e7c1c62a747da4bf5cedff8b6378de
gbenroofficial <68187993+gbenroofficial@users.noreply.github.com>
2022-08-02 01:48:59 +0100

    Add files via upload

---
+++ ItemSet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daniel_s_UI_prototype
+{
+    class ItemSet
+    {
+        private IList<string> items;
+        private float support = 0;
+        private IList<string> ids;
+
+        public IList<string> Ids { get => ids; set => ids = value; }
+        public IList<string> Items { get => items; set => items = value; }
+        public float Support { get => support; set => support = value; }
+    }
+}
---
+++ Rule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Collections;
+using System.Reflection;
+
+
+namespace Daniel_s_UI_prototype
+{
+    class Rule
+    {
+        private ItemSet Antecedent;
+        private ItemSet Consequent;
+        private float Lift = 0;
+        private float Confidence = 0;
+        private float Support = 0;
+
+        public ItemSet antecedent { get => Antecedent; set => Antecedent = value; }
+
+        public IList<string> getAnte { get => Antecedent.Items; set => Antecedent.Items = value; }
+        public IList<string> getConse { get => Consequent.Items; set => Consequent.Items = value; }
+        public ItemSet consequent { get => Consequent; set => Consequent = value; }
+        public float lift { get => Lift; set => Lift = value; }
+        public float confidence { get => Confidence; set => Confidence = value; }
+        public float support { get => Support; set => Support = value; }
+    }
+}
